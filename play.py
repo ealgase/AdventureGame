c5df31293549feb0d747b1f1aa178a2c6e7e018d
MCJack123 <jackbruienne@yahoo.com>
2015-12-11 10:19:34 -0500

    Update play.py

--- play.py
+++ play.py
@@ -9,7 +9,7 @@ import time
 import random
 import math
 def centerText(text, sidech):
-    spacesOnSide = width - text.length - (sidech.length * 2)
+    spacesOnSide = width - text.len() - (sidech.len() * 2)
     appendSpace = " "
     i = 0
     while i <= width:
